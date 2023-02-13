0c814ff258a5e54a8c4d6e1045d12316be5935e3
Thomas Grenier <grenier.tjm@gmail.com>
2023-02-13 19:04:07 +0100

    insert a product and its details in product page

--- front/js/product.js
+++ front/js/product.js
@@ -10,19 +10,45 @@ if (searchParams.has('id')){
 
 //on récupère les données de l'api correspondant a l'id du produit
 fetch("http://localhost:3000/api/products/" + productId)
-.then(response => {
-    //traitement de la réponse
-    //tester si on obtient la reponse attendu
-    if (response.ok) {
-        // on sait que tout va bien testant quand le code de reponse est >= 200 ou si response.ok
-        return response.json();
-    }
+    .then(response => {
+        //traitement de la réponse
+        //tester si on obtient la reponse attendu
+        if (response.ok) {
+            // on sait que tout va bien testant quand le code de reponse est >= 200 ou si response.ok
+            return response.json();
+        }
 
-    //Sinon on vérifie que le produit existe
-    if (response.status === 404) {
-        throw new Error("Le produit n'existe pas");
-    }
+        //Sinon on vérifie que le produit existe
+        if (response.status === 404) {
+            throw new Error("Le produit n'existe pas");
+        }
 
-    //Sinon en déclanche une erreur
-    throw new Error('Serveur injoignable, veuillez réessayez ultérieurement');
-})
+        //Sinon en déclanche une erreur
+        throw new Error('Serveur injoignable, veuillez réessayez ultérieurement');
+    })
+    .then(kanap => {
+        //traitement des données de la reponse 
+        //création des balises
+        const img = document.createElement("img");
+
+        //insertion des éléments
+        img.src= kanap.imageUrl;
+        img.alt= kanap.altTxt;
+
+        //affichage des éléments
+        document.querySelector(".item__img").appendChild(img);
+        document.querySelector("#title").textContent= kanap.name;
+        document.querySelector("#price").textContent= kanap.price;
+        document.querySelector("#description").textContent= kanap.description;
+
+        //affichage des couleurs
+        for (color of kanap.colors){
+            let option = document.createElement("option");
+            option.value= color;
+            option.textContent = color;
+            document.querySelector("#colors").appendChild(option);
+        }
+    })
+    .catch(error => {
+        alert(error.message);
+    });
